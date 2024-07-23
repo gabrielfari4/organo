95c4ba5c379f7403932d7e60f4792a9f7c878f70
Gabriel Faria <gabrielfaria88@gmail.com>
2024-07-23 17:28:05 -0300

    creating footer component

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,21 @@
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <section className='footer'>
+            <div className='icons'>
+                <img src="/images/fb.png" alt="Logo Facebook" />
+                <img src="/images/tw.png" alt="Logo Twitter/X" />
+                <img src="/images/ig.png" alt="Logo Instagram" />
+            </div>
+            <div className='logo'>
+                <img src="/images/logo.png" alt="Logo Organo" />
+            </div>
+            <div className='text'>
+                <p>Desenvolvido por Alura.</p>
+            </div>
+        </section>
+    )
+}
+
+export default Footer;
