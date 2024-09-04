6f5509c1e5ba133f1b4f76218b7559440c5adb5a
Gabriel Faria <gabrielfaria88@gmail.com>
2024-09-04 15:17:57 -0300

    feat: implementing icon when github profile page not filled

--- src/components/Employee/index.jsx
+++ src/components/Employee/index.jsx
@@ -1,6 +1,8 @@
 import './styles.css'
 import { IoMdCloseCircle } from "react-icons/io";
 import { MdFavorite, MdFavoriteBorder } from "react-icons/md";
+import { FaUserCircle } from "react-icons/fa";
+import {Img} from 'react-image'
 
 const Employee = (props) => {
 
@@ -13,17 +15,26 @@ const Employee = (props) => {
         onClick: favorite
     }
 
+    const userImg = () => {
+        const imgUrl = `http://github.com/${props.image}.png`
+        return imgUrl
+    }
+
+    
+
     return (
         <div className='colaborador'>
-            
             <IoMdCloseCircle 
                 size={25} 
                 className='deletar' 
                 onClick={() => props.onDeleting(props.id)}
             />
             <div className='cabecalho' style={{ backgroundColor: props.primaryColor }}>
-                {/* TODO: criar lógica para quando não houver foto */}
-            <img src={`http://github.com/${props.image}.png`} alt={`Foto ${props.name}`} />
+            {props.image
+                ? <img src={userImg()} alt='' /> 
+                : <FaUserCircle className='iconUser' size={100} style={{ backgroundColor: 'white' }}/>
+            }
+            
             </div>
             <div className='rodape'>
                 <h4>{props.name}</h4>
